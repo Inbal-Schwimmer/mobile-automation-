d0fabdaf0713d28324c616623abf7bfdccd179b7
Inbal-Schwimmer <inbal.schwimmer@remepy.com>
2024-12-22 16:47:50 +0200

    working with platform type
    Added the option to work with platform types - iOs and Android

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,8 +2,9 @@ import pytest
 from utils.driver_factory import DriverFactory
 
 @pytest.fixture
-def setup_driver(request):
-    platform = request.param  # Get platform from test param
+def setup_driver(platform):
+# def setup_driver(request):
+    # platform = request.param  # Get platform from test param
     driver = DriverFactory.create_driver(platform)  # Initialize driver based on the platform
     yield driver
     driver.quit()
--- utils/driver_factory.py
+++ utils/driver_factory.py
@@ -12,17 +12,19 @@ class DriverFactory:
             options = UiAutomator2Options()
             options.platformName = "Android"
             options.deviceName = "emulator-5554"
-            options.app = "/Users/inbalschwimmershafir/projects/universal.apk"  # APK path
-            options.automationName = "UiAutomator2"  # Automation engine
+            options.app = "/Users/inbalschwimmershafir/Documents/GitHub/app-base-debug.apk"  # APK path
+            # options.automationName = "UiAutomator2"  # Automation engine
+            options.automationName = "Flutter"
             options.appPackage = "com.remepy.devbct"
             options.appActivity = "com.remepy.remepy.MainActivity"
         elif platform == "iOS":
             options = XCUITestOptions()
             options.platformName = "iOS"
-            options.deviceName = "iPhone Simulator"  # example need to update
-            options.app = "/path/to/your/app.ipa"  # example need to update
-            options.automationName = "XCUITest"  # example need to update
-            options.bundleId = "com.remepy.iosapp"  # example need to update
+            options.deviceName = "iPhone15"  # example need to update
+            options.app = "/Users/inbalschwimmershafir/Documents/GitHub/Runner.app"  # example need to update
+            options.automationName = "Flutter"
+            # options.automationName = "XCUITest"
+            options.bundleId = "com.remepy.devbct"  # example need to update
         else:
             raise ValueError(f"Unknown platform: {platform}")
 
